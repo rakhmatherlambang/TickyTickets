c675315806122e3d75f5cd36eb96413a261d6573
Rakhmat Herlambang <rakhmat.herlambang16@gmail.com>
2024-12-02 09:58:26 +0700

    migrate product table and product img

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,44 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'product_name',
+        'description',
+        'category_id',
+        'subcategory_id',
+        'store_id',
+        'regular_price',
+        'discounted_price',
+        'tax_rate',
+        'stock_quantity',
+        'stock_status',
+        'stock_status',
+        'visibility',
+        'meta_title',
+        'meta_description',
+        'status',
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function subcategory(){
+        return $this->belongsTo(Subcategory::class);
+    }
+    public function strore(){
+        return $this->belongsTo(store::class);
+    }
+    public function seller(){
+        return $this->belongsTo(User::class);
+    }
+    public function images(){
+        return $this->hasMany(ProductImage::class);
+    }
 }
